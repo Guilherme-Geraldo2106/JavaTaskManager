49f6576d1ffcdc06238bb79035944d4b71d72552
Guilherme-Geraldo2106 <guilherme.geraldo2106@gmail.com>
2024-12-27 22:02:14 -0300

    start

--- src/main/java/com/JavaTaskManager/JavaTaskManager/Dto/LoginRequestModel.java
+++
@@ -1,8 +0,0 @@
-package com.JavaTaskManager.JavaTaskManager.Dto;
-
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequestModel(@NotBlank String username, @NotBlank String password) {
-
-}
--- src/main/java/com/JavaTaskManager/JavaTaskManager/Dto/UpdateTaskRequestModel.java
+++
@@ -1,7 +0,0 @@
-package com.JavaTaskManager.JavaTaskManager.Dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UpdateTaskRequestModel(@NotBlank String title, @NotBlank String description, boolean completed) {
-
-}
